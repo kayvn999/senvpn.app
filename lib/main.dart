a99f3feb76f2fb6f108998492ccecd8ac16af625
kayvn999 <raustorevip@gmail.com>
2026-04-22 14:36:54 +0700

    add: firebase_options.dart and fix initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'core/ads/admob_service.dart';
 import 'core/firebase/remote_config_service.dart';
@@ -29,7 +30,7 @@ void main() async {
   ));
 
   // Firebase
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Crashlytics
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
